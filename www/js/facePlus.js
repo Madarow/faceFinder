9ecb74b5086f52219f65760244ae887d792a07b9
philemoncombes <philemon.combe@gmail.com>
2017-03-20 09:57:10 +0100

    beautify code

--- www/js/facePlus.js
+++ www/js/facePlus.js
@@ -1,17 +1,17 @@
 angular.module('starter.facePlus', [])
 
-.factory('facePlus', function($q,$http,$rootScope) {
+  .factory('facePlus', function($q, $http, $rootScope) {
 
-  var facePpAPI = {
-    ENDPOINT : 'https://api-us.faceplusplus.com/facepp/v3/',
-    secret: 'sBbdR05BdbyTo8GOnrxCYKYWb22UCgJ-',
-    key: 'sBXaPtWQnwTXXDqRBJ7f9p2DYij4w0nE'
-  };
+    var facePpAPI = {
+      ENDPOINT: 'https://api-us.faceplusplus.com/facepp/v3/',
+      secret: 'sBbdR05BdbyTo8GOnrxCYKYWb22UCgJ-',
+      key: 'sBXaPtWQnwTXXDqRBJ7f9p2DYij4w0nE'
+    };
 
-  var getDataUri = function(url, callback) {
-    var image = new Image();
+    var getDataUri = function(url, callback) {
+      var image = new Image();
 
-    image.onload = function () {
+      image.onload = function() {
         var canvas = document.createElement('canvas');
         canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
         canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
@@ -23,32 +23,32 @@ angular.module('starter.facePlus', [])
 
         // ... or get as Data URI
         callback(canvas.toDataURL('image/png'));
-    };
-
-    image.src = url;
-}
-
-  return {
-    doDetect:function(opts){
-      // Usage
-
-      options = {
-         fileKey: "image_file",
-         fileName: 'img_test',
-         chunkedMode: false,
-         mimeType: "image/png"
       };
-      getDataUri(opts.image_url, function(dataUri) {
-          url = facePpAPI.ENDPOINT+'detect?api_secret='+facePpAPI.secret+'&api_key='+facePpAPI.key+'&return_attributes=gender,age';
-          ft = new FileTransfer
-          ft.upload(opts.image_url,encodeURI(url),function(r){
-            console.log(r);
-          },function(e){
-            console.log(e);
-          },options)
-      });
 
+      image.src = url;
     }
-  }
 
-});
+    return {
+      doDetect: function(opts) {
+        // Usage
+
+        options = {
+          fileKey: "image_file",
+          fileName: 'img_test',
+          chunkedMode: false,
+          mimeType: "image/png"
+        };
+        getDataUri(opts.image_url, function(dataUri) {
+          url = facePpAPI.ENDPOINT + 'detect?api_secret=' + facePpAPI.secret + '&api_key=' + facePpAPI.key + '&return_attributes=gender,age';
+          ft = new FileTransfer
+          ft.upload(opts.image_url, encodeURI(url), function(r) {
+            console.log(r);
+          }, function(e) {
+            console.log(e);
+          }, options)
+        });
+
+      }
+    }
+
+  });
